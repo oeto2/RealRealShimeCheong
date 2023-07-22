27baa6533f1183af60f0a5e008771fc7ca7e4d29
oeto2 <oeto2@naver.com>
2023-07-22 20:16:37 +0900

    캘린더 및 해시계 애니메이션 리얼타임 적용
    하루는 300초

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
+    //캘린더의 애니메이터
+    public Animator animator_Celender;
+
     //시간을 표시할 텍스트
     public Text text_TimeText;
 
@@ -16,16 +19,45 @@ public class TimeManager : MonoBehaviour
     [SerializeField]
     private int timeMultipleCation;
 
+    //하루가 지나기위해 걸리는시간
+    public int int_DayMinute;
+
+    //몇일 째인지 확인하는 변수
+    [SerializeField]
+    private int int_DayCount = 1;
+
+    public float Timetiem;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        //실제 시간 = 시간 + 배속
         float_RealTime += Time.deltaTime * timeMultipleCation;
         text_TimeText.text = float_RealTime.ToString("F0");
+
+        //만약 int_DayMinute 만큼 시간이 진행되었을경우
+        if (int_DayMinute <= float_RealTime)
+        {
+            //날짜 증가
+            int_DayCount++;
+
+            //시간 초기화
+            float_RealTime = 0;
+
+            //캘린더 애니메이션 진행
+            NextDayAnimaton(int_DayCount);
+        }
+    }
+
+    //날짜가 지났을때 실행할 애니메이션
+    public void NextDayAnimaton(int _day)
+    {
+        //캘린더 날짜 바꾸기
+        animator_Celender.SetInteger("DayNum", _day);
     }
 }
