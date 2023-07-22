2f18be52d9dbd2fa7c86de3184d0196b80a59660
CielSsongsSeta <gch00536@naver.com>
2023-07-23 02:44:15 +0900

    NPC 기본 대화 완성

---
+++ Assets/Scripts/Dialog/Dialog_TypingWriter_Songnara.cs
@@ -0,0 +1,129 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog_TypingWriter_Songnara : MonoBehaviour
+{
+    // 실제 채팅이 나오는 텍스트
+    public Text ChatText;
+
+    // 캐릭터 이름이 나오는 텍스트
+    public Text CharacterName;
+
+    // 대화를 빠르게 넘길 수 있는 키(default : space)
+    public List<KeyCode> skipButton;
+
+    public string writerText = "";
+
+    public string characternameText = "";
+
+    bool isButtonClicked = false;
+
+    public bool bool_isNPC = false;
+
+    public GameObject images_NPC;
+
+    //public Sprite[] images_NPC_portrait;
+
+    public Sprite images_NPC_portrait;
+
+
+    //최초 클릭
+    void Start()
+    {
+        StartCoroutine(TextPractice());
+        //TextPractice();
+        //StopCoroutine(TextPractice());
+        CharacterName.text="";
+    }
+
+
+    void Update()
+    {
+        foreach (var element in skipButton) // 버튼 검사
+        {
+            if (Input.GetKeyDown(element))
+            {
+                isButtonClicked = true;
+            }
+        }
+        TextPractice();
+        //StopCoroutine(TextPractice());
+
+        /* if (Input.GetMouseButtonDown(0))
+         {
+             StartCoroutine(TextPractice());
+         }*/
+
+        /*if (Input.GetMouseButtonDown(1))
+                {
+                    StopCoroutine(TextPractice());
+                }*/
+    }
+
+
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("이건 Touch! 송나라 상인!!!!");
+            StartCoroutine(TextPractice());
+            //bool_isBotjim = true;
+            if (bool_isNPC == true)
+            {
+                images_NPC.SetActive(true);
+                bool_isNPC = false;
+
+                //GameObject.Find("NPC_Profile").GetComponent<Image>().sprite = images_NPC_portrait[1];
+                GameObject.Find("NPC_Profile").GetComponent<Image>().sprite = images_NPC_portrait;
+            }
+            else
+            {
+                images_NPC.SetActive(false);
+               // images_NPC_portrait.SetActive(false);
+                bool_isNPC = true;
+                StopCoroutine(TextPractice());
+            }
+        }
+    }
+
+    IEnumerator NormalChat(string narrator, string narration)
+    {
+        int a = 0;
+        CharacterName.text = narrator;
+        //characternameText = narrator;
+        writerText = "";
+
+        //narrator = CharacterName.text;
+
+        //텍스트 타이핑
+        for (a = 0; a < narration.Length; a++)
+        {
+            writerText += narration[a];
+            ChatText.text = writerText;
+
+            //텍스트 타이핑 시간 조절
+            //yield return null;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        //키(default : space)를 다시 누를 때까지 무한정 대기
+        while (true)
+        {
+            if (isButtonClicked)
+            {
+                isButtonClicked = false;
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TextPractice()
+    {
+        yield return StartCoroutine(NormalChat(characternameText, writerText));
+        //yield return StartCoroutine(NormalChat(characternameText, writerText));
+        //yield return StartCoroutine(NormalChat("나는봇짐", "?안녕하세요, 반갑습니다. 대화 전환 테스트입니다 이것은 테스트지? 그럼 테스트지 테스트야 테스트군 테스트"));
+    }
+}
