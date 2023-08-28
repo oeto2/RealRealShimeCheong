fdb6a0b9a7c3fca0fa019f83ed6faa74c1eadfc4
oeto2 <oeto2@naver.com>
2023-08-28 09:46:31 +0900

    다이얼로그 Canvas 카메라 설정값 변경

---
+++ Assets/Scripts/UI/Message/ToastMessage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToastMessage : MonoBehaviour
+{
+    //슬롯 UI SpriteRen
+    public SpriteRenderer spriteRen_Slot;
+
+    //오브젝트 UI SpriteRen
+    public SpriteRenderer spriteRen_Object;
+
+    //TosastMessage Text
+    public Text text_Toast;
+
+    //Object Name Text
+    public Text text_ObjectName;
+
+    //아이템 sprite 이미지
+    public Sprite[] sprite_Items;
+
+    //단서 Sprite 이미지
+    public Sprite[] sprite_Clues;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
